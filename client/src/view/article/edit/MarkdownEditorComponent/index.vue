3406404a0f975797efc28cba402abb680a271007
nicolaseberle <nicolas.eberle.09@gmail.com>
2019-01-31 17:24:36 +0100

    fix some bugs

--- client/src/view/article/edit/MarkdownEditorComponent/index.vue
+++ client/src/view/article/edit/MarkdownEditorComponent/index.vue
@@ -19,25 +19,70 @@
 
 <script>
 import MarkdownEditor from '../../../../components/MarkdownEditor'
+import { mapGetters } from 'vuex'
+import axios from 'axios'
+
+
+const defaultForm = {
+  status: 'draft',
+  title: '',
+  abstract: '',
+  content: '',
+  arr_content: [{
+                  name:"titre_1",
+                  title:"Titre 1",
+                  title_placeholder:"Titre 1",
+                  content:"Type your text",
+                  path_figure: "",
+                  display:true
+                }],
+  content_short: '',
+  source_uri: '',
+  image_uri: '',
+  display_time: undefined,
+  id: undefined,
+  platforms: ['a-platform'],
+  comment_disabled: false,
+  importance: 0
+}
 
 const content = `
-# Titre 1
-## Titre 2
+# Article Title
+
+## Abstract
+
+Firstly, an abstract of your article.
+
+## Titre 1
+### Titre 1.1
 
 This is a paragraph
 
-* vue
-* element
-* webpack
+List example:
+* First element
+* Second element
+* Third element
 `
 export default {
   name: 'MarkdownDemo',
   components: { MarkdownEditor },
   data() {
     return {
+      postForm: {},
       content: content,
       html: ''
     }
+  },
+  computed: {
+    ...mapGetters(['sidebar']),
+    closeSidebar() {
+      this.sidebar.opened = false
+    }
+  },
+  created () {
+    this.sidebar.opened = false
+    this.id = this.$route.params && this.$route.params.id
+    this.fetchData(this.id)
   },
   mounted () {
     import('showdown').then(showdown => {
@@ -50,6 +95,13 @@ export default {
       import('showdown').then(showdown => {
         const converter = new showdown.Converter()
         this.html = converter.makeHtml(this.content)
+      })
+    },
+    fetchData(id) {
+      axios.get('/api/articles/' + id ).then(response => {
+        this.postForm = response.data
+      }).catch(err => {
+        console.log(err)
       })
     }
   }
