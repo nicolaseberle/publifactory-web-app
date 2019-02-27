02c6330e9481640b0c9d9a2884c621a24c9c9032
nicolaseberle <nicolas.eberle.09@gmail.com>
2019-02-27 19:25:48 +0100

    check some elements

--- client/src/view/article/edit/index.vue
+++ client/src/view/article/edit/index.vue
@@ -2,35 +2,36 @@
   <div>
   <div class="components-container-article">
     <el-row type="flex" class="row-bg" justify="space-between" style="margin-bottom:20px;background-color:white" :gutter="40">
-      <el-col :span="10">
+      <el-col :span="12">
         <el-button-group>
-          <el-button v-if="valueTypeEditor==1" type="primary"  @click="changeEditor('LightEditor')">Light Editor</el-button>
-          <el-button v-if="valueTypeEditor!=1" type=""  @click="changeEditor('LightEditor')">Light Editor</el-button>
-          <el-button v-if="valueTypeEditor==2" type="primary"  @click="changeEditor('MarkdownEditor')">Rich Text</el-button>
-          <el-button v-if="valueTypeEditor!=2" type=""  @click="changeEditor('MarkdownEditor')">Rich Text</el-button>
-          <el-button v-if="valueTypeEditor==3" type="primary"  @click="changeEditor('LatexEditor')">Latex</el-button>
-          <el-button v-if="valueTypeEditor!=3" type=""  @click="changeEditor('LatexEditor')">Latex</el-button>
+          <el-button v-if="valueTypeEditor==1" type="primary" round  @click="changeEditor('LightEditor')">Light Editor</el-button>
+          <el-button v-if="valueTypeEditor!=1" type="" round @click="changeEditor('LightEditor')">Light Editor</el-button>
+          <el-button v-if="valueTypeEditor==2" type="primary" round @click="changeEditor('MarkdownEditor')">Rich Text</el-button>
+          <el-button v-if="valueTypeEditor!=2" type="" round @click="changeEditor('MarkdownEditor')">Rich Text</el-button>
+          <el-button v-if="valueTypeEditor==3" type="primary" round @click="changeEditor('LatexEditor')">Latex</el-button>
+          <el-button v-if="valueTypeEditor!=3" type="" round @click="changeEditor('LatexEditor')">Latex</el-button>
         </el-button-group>
-        <el-dropdown split-button type="primary" >
-          <!--<svg-icon icon-class='history-clock'/><span style='margin-left:10px'>Version </span>-->
-          Version
+        <el-dropdown trigger="click" class="international" @command="actionHandleCommand">
+          <div>
+            <el-button class="el-button-action" round>Version<i class="el-icon-arrow-down" style='margin-left:10px'/></el-button>
+          </div>
           <el-dropdown-menu slot="dropdown">
-            <el-dropdown-item>Submited</el-dropdown-item>
-            <el-dropdown-item>Reviewed - 01/08/2018</el-dropdown-item>
-            <el-dropdown-item>Reviewed - 10/09/2018</el-dropdown-item>
-            <el-dropdown-item>Reviewed - 15/09/2018</el-dropdown-item>
+            <el-dropdown-item command="v1">Submited</el-dropdown-item>
+            <el-dropdown-item command="v2">Reviewed - 01/08/2018</el-dropdown-item>
+            <el-dropdown-item command="v3">Reviewed - 10/09/2018</el-dropdown-item>
+            <el-dropdown-item command="v4">Reviewed - 15/09/2018</el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
       </el-col>
-      <el-col :span="8">
-
-        <el-button-group>
-
-          <el-button v-if="(valueTypeEditor==2 || valueTypeEditor==3) && flagHidePDF==0" type="" @click="handleHidePDF()"  round>Hide PDF</el-button>
-          <el-button v-if="(valueTypeEditor==2 || valueTypeEditor==3) && flagHidePDF==1" type="" @click="handleHidePDF()"  round>Hide PDF</el-button>
-          <el-button type="" round disabled>Download the article</el-button>
-          <el-button type="" round >Submit your article</el-button>
-        </el-button-group>
+      <el-col :span="12">
+        <div style='text-align:right'>
+          <el-button-group>
+            <el-button v-if="(valueTypeEditor==2 || valueTypeEditor==3) && flagHidePDF==0" type="" @click="handleHidePDF()"  round>Hide PDF</el-button>
+            <el-button v-if="(valueTypeEditor==2 || valueTypeEditor==3) && flagHidePDF==1" type="" @click="handleHidePDF()"  round>Hide PDF</el-button>
+            <el-button type="" round disabled>Download the article</el-button>
+            <el-button type="" round >Submit your article</el-button>
+          </el-button-group>
+        </div>
       </el-col>
       <!--<el-col v-if="postForm.status == 'Reviewing'" :span="4" :offset="18"><el-button type="warning">Edit this version</el-button></el-col>-->
     </el-row>
@@ -77,6 +78,9 @@ export default {
   mounted() {
   },
   methods: {
+    actionHandleCommand (action) {
+
+    },
     handleHidePDF () {
       if(this.flagHidePDF == 1){
         this.flagHidePDF = 0
