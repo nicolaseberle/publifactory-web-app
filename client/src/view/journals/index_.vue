3ee72b25ec0f02e4b319fccedcfc0e546340ea09
Nicolas <nicolas.eberle.09@gmail.com>
2019-06-10 12:19:39 +0200

    update the createJournal view

---
+++ client/src/view/journals/index_.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{test}} {{journalId}}
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      test : 'on est sur la page du journal',
+      journalId: ''
+    }
+  },
+  created (){
+    this.journalId = this.$route.params && this.$route.params.id
+  }
+}
+</script>
