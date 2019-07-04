fdd29cd67a5f9c8cdf3acb1bf79e17060224d10a
Nicolas <nicolas.eberle.09@gmail.com>
2019-07-04 18:05:47 +0200

    add a loading icon to avoid unexpected action

--- client/src/components/NProgress.vue
+++ client/src/components/NProgress.vue
@@ -4,10 +4,14 @@
   import nprogress from 'nprogress'
   import { mapGetters } from 'vuex'
   import 'nprogress/nprogress.css'
+  import { Loading } from 'element-ui';
 
   export default {
   props: {
     parent: String
+  },
+  data: {
+    loadingInstance: Object
   },
   computed: {
     ...mapGetters(['loggedIn'])
@@ -16,23 +20,26 @@
     console.log("nprogress :: ", this.parent)
     if(this.loggedIn === true)
     if (this.parent) {
-      nprogress.configure({ parent: this.parent })
+      //nprogress.configure({ parent: this.parent })
     }
+
     console.log("nprogress :: ",nprogress)
     this.$router.beforeEach((to, from, next) => {
-      nprogress.start()
+      this.loadingInstance = Loading.service({ target: '#app-main' });
+      //nprogress.start()
       next()
     })
     this.$router.afterEach(() => {
-      nprogress.done()
-    })
+      //nprogress.done()
+      this.loadingInstance.close();
+    })/*
     Vue.http.interceptors.push((request, next) => {
       nprogress.start()
       next(response => {
         nprogress.done()
         return response
       })
-    })
+    })*/
   }
 }
 </script>
--- client/src/view/layout/components/AppMain.vue
+++ client/src/view/layout/components/AppMain.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="app-main">
+  <section class="app-main" id="app-main">
     <transition name="fade-transform" mode="out-in">
         <router-view :key="key"/>
     </transition>
