156a505104420e04ce840c078c1f02fe7059699b
Léo Riberon-Piatyszek <leo.riberon-piatyszek@epitech.eu>
2019-07-09 11:09:55 +0200

    Improve socketio

--- server/config/socketio.js
+++ server/config/socketio.js
@@ -22,9 +22,9 @@ module.exports = function (io) {
     mapUser[socket.id] = new User(socket.id)
 
     socket.on('disconnect', () => {
-      console.log('[socket.io] AN USER JUST DISCONNECTED: %s', mapUser[socket.id].id)
+      console.log('[socket.io] AN USER JUST DISCONNECTED: %s', mapUser[socket.id].id);
       delete mapUser[socket.id];
-    })
+    });
 
     /**
      * Data contain :
@@ -34,15 +34,17 @@ module.exports = function (io) {
      * - numSubSubBlock
      */
     socket.on('UPDATE_SECTION', (data) => {
-      socket.broadcast.emit(`UPDATE_BLOCK`, data)
-    })
+      socket.to(mapUser[socket.id].idArticle).emit(`UPDATE_BLOCK`, data)
+    });
 
     /**
      * Data contain id_article
+     * We put the user socket in a room to broadcast just only specific users
      */
     socket.on('SET_ARTICLE', (data) => {
-      mapUser[socket.id].setArticleId(data.id_article)
+      mapUser[socket.id].setArticleId(data.id_article);
+      socket.join(data.id_article);
       console.log('[socket.io] AN USER WORKS ON ARTICLE %s: %s', data.id_article, mapUser[socket.id].id)
-    })
+    });
   })
 };
