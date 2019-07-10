65aca1dc6ea9d96583dcff1b012868a41abcddca
Nicolas <nicolas.eberle.09@gmail.com>
2019-07-10 09:50:07 +0200

    update editor dashboard

--- client/src/view/dashboard/editor/DataTableStatus.vue
+++ client/src/view/dashboard/editor/DataTableStatus.vue
@@ -1,71 +1,90 @@
 <template>
-  <div class="app-container">
-    <content-module name="articles">
-      <el-row :gutter="20">
-        <el-col :span='6'>
-          <el-button-group>
-            <el-button round v-on:click="createArticle()">Create Article</el-button>
-            <el-button round v-on:click="importArticle()">Import</el-button>
-          </el-button-group>
-        </el-col>
-      </el-row>
-      <div class='dashboard-tab'>
-      <div style='margin-top:20px;z-index:1000;'>
-      <el-tabs v-model="activeName" @tab-click="handleClick">
-      <el-tab-pane label="All" name="first">
-        <dataTableStatus desiredstatus="All"/>
-      </el-tab-pane>
-      <el-tab-pane label="Submission" name="second">
-        <dataTableStatus desiredstatus="Submited"/>
-      </el-tab-pane>
-      <el-tab-pane name="third">
-        <span slot="label">Review<el-badge :value="2" lass="mark"/></span>
-        <dataTableStatus desiredstatus="Reviewing"/>
-      </el-tab-pane>
-      <el-tab-pane label="CopyEditing" name="fourth">Copy Editing</el-tab-pane>
-      <el-tab-pane label="Production" name="fifth">Production</el-tab-pane>
-  </el-tabs>
-  </div>
-  </div>
-  <el-dialog
-    title="Add Reviewer"
-    :visible.sync="flagAddReviewer"
-    width="70%">
-    <addReviewer v-if="flagAddReviewer" :article_id="selectedArticleId" v-on:close="flagAddReviewer = false"/>
-    <!--<span slot="footer" class="dialog-footer">
-      <el-button v-on:click="flagAddReviewer=false" round>Cancel</el-button>
-      <el-button type="primary" v-on:click="flagAddReviewer = false" round>Validate</el-button>
-    </span>-->
-  </el-dialog>
-</content-module>
+  <div>
+    <data-table ref="articles" @page-change="fetchArticles" >
+      <el-table :data="articles" @row-click="setSelectedRow" fit highlight-current-row style="width: 100%">
+      <el-table-column class-name="date-col" width="140px" label="Date">
+        <template slot-scope="scope">
+          <span>{{ scope.row.creationDate | moment("DD/MM/YYYY") }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column min-width="300px"  label="Title">
+        <template slot-scope="scope">
+          <router-link :to="'/articles/'+scope.row.id" class="link-type">
+            <span>{{ scope.row.title }}</span>
+          </router-link>
+        </template>
+      </el-table-column>
+      <el-table-column class-name="author-col" width="120px"  label="Author">
+        <template slot-scope="articles">
+          <div v-for="item_author in articles.row.authors">
+            {{ item_author.author.firstname[0] }}. {{ item_author.author.lastname }}
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column class-name="author-col" width="120px"  label="Ass. Editor">
+        <template slot-scope="articles">
+          <div v-for="associate_editor in articles.row.associate_editor">
+            {{ associate_editor.firstname[0] }}. {{ associate_editor.lastname }}
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column class-name="author-col" width="120px"  label="Reviewer">
+        <template slot-scope="articles" >
+          <div v-for="reviewer in articles.row.reviewers" >
+              <span style="white-space: pre;">{{ reviewer.firstname[0] }}. {{ reviewer.lastname }}</span>
+          </div>
+        </template>
+      </el-table-column>
+      <el-table-column class-name="doi-col" label="DOI" width="50">
+        <template v-if="scope.row.doi" slot-scope="scope">
+          <i class="el-icon-check"></i>
+        </template>
 
-<!--
-<el-dialog :title="Titre" :visible.sync="formVisible">
-  <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
-      <el-form-item :label="$t('article.title')">
-        <el-input v-model="temp.title"/>
-      </el-form-item>
-  </el-form>
-  <span slot="footer" class="dialog-footer">
-    <el-button @click="closeCreationDialog()" round>Cancel</el-button>
-    <el-button type="primary" @click="dialogPvVisible = false" round>Validate</el-button>
-  </span>
-</el-dialog>-->
-
-</div>
+      </el-table-column>
+      <el-table-column class-name="status-col" label="Status" width="110">
+        <template slot-scope="scope">
+          <el-tag class-name="el-tag-status"  :type="scope.row.status | statusFilter" >{{ scope.row.status }}</el-tag>
+        </template>
+      </el-table-column>
+      <el-table-column class-name="action-col"  label="Actions" width="120">
+        <template slot-scope="scope">
+          <!--<router-link :to="'/example/edit/'+scope.row.id">-->
+            <!--<el-button type="primary" size="small" icon="el-icon-edit">Edit</el-button>-->
+            <el-dropdown trigger="click" class="international" @command="actionHandleCommand">
+              <div>
+                <el-button class="el-button-action" icon="el-icon-more" circle>
+                </el-button>
+              </div>
+              <el-dropdown-menu slot="dropdown" >
+                <el-dropdown-item  command="settings" disabled>Access & settings</el-dropdown-item>
+                <el-dropdown-item  command="openArticle">Open the article</el-dropdown-item>
+                <el-dropdown-item  command="assignReviewer" >Assign a reviewer</el-dropdown-item>
+                <el-dropdown-item  command="sendEmailToAuthors">Send an email to authors</el-dropdown-item>
+                <el-dropdown-item  command="historicalActions">View historical actions</el-dropdown-item>
+                <el-dropdown-item  command="referee">Referee</el-dropdown-item>
+                <el-dropdown-item  command="survey">Survey (Scopus, Google Scholar...)</el-dropdown-item>
+                <el-dropdown-item  command="remove" style="{color:'red'}" disabled>Remove the article</el-dropdown-item>
+              </el-dropdown-menu>
+            </el-dropdown>
+          <!--</router-link>-->
+        </template>
+      </el-table-column>
+    </el-table>
+    </data-table>
+  </div>
 </template>
 <script>
 import { mapGetters } from 'vuex'
+import DataTable from '../../../components/DataTable'
 import locales from '../../../locales/article'
 import axios from 'axios'
 import addReviewer from '../../../components/Reviewer'
-import dataTableStatus from './DataTableStatus'
-
-const debug = require('debug')('frontend')
-var uuidv4 = require('uuid/v4');
 
 export default {
   locales,
+  props: {
+    desiredstatus : String
+  },
   data () {
     return {
       flagVisibleAddReviewer: false,
@@ -99,7 +118,7 @@ export default {
     ])
   },
   components: {
-    dataTableStatus,
+    DataTable,
     addReviewer
   },
   methods: {
@@ -131,7 +150,11 @@ export default {
       axios.get('/api/articles/', {
         headers: {'Authorization': `Bearer ${this.accessToken}`}
       }).then(list => {
-        this.articles = list.data.articles.filter(d => d.status !== 'Draft')
+        if(this.desiredstatus === 'All')
+          this.articles = list.data.articles.filter(d => d.status !== 'Draft')
+        else
+          this.articles = list.data.articles.filter(d => d.status === this.desiredstatus)
+          
       }).catch(err => {
         console.error(err)
       })
@@ -213,15 +236,3 @@ export default {
   }
 }
 </script>
-<style>
-.dashboard-tab{
-  padding-top:20px
-}
-
-.tabs, .el-tabs__nav{
-    padding-top:10px
-}
-.el-table__header-wrapper{
-  white-space: pre-line;
-}
-</style>
