e5bdca36a40a19f047930eb56919c79bbed4e345
Léo Riberon-Piatyszek <leo.riberon-piatyszek@epitech.eu>
2019-09-04 17:53:12 +0200

    README.md updated and some docs

--- server/routes.js
+++ server/routes.js
@@ -2,33 +2,37 @@
  * Main application routes
  */
 
-'use strict'
+'use strict';
 
-var errors = require('./components/errors')
-var config = require('../config').backend
-var path = require('path')
+const path = require('path');
+const YAML = require('yamljs');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = YAML.load(path.join(__dirname, './config/swagger.yaml'));
 
 const jwtCheck = require('./auth/jwt');
 
 module.exports = function (app) {
+  // Create documentation route
+  app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
   // Insert routes below
-  app.use('/api/users', require('./api/user'))
-  app.use('/api/auth', require('./auth'))
-  app.use('/api/invitations', require('./api/invitations'))
+  app.use('/api/users', require('./api/user'));
+  app.use('/api/auth', require('./auth'));
+  app.use('/api/invitations', require('./api/invitations'));
 
   app.use(function(req, res, next) {
     jwtCheck(req, res, next);
-  })
+  });
 
-  app.use('/api/articles', require('./api/article'))
-  app.use('/api/journals', require('./api/journal'))
-  app.use('/api/comments', require('./api/comment'))
-  app.use('/api/data', require('./api/data'))
-  app.use('/api/figure', require('./api/figure'))
-  app.use('/api/converter', require('./Converter'))
-  app.use('/api/roles', require('./api/roles'))
-  app.use('/api/pictures', require('./api/picture'))
-  app.use('/api/history', require('./api/article/history'))
+  app.use('/api/articles', require('./api/article'));
+  app.use('/api/journals', require('./api/journal'));
+  app.use('/api/comments', require('./api/comment'));
+  app.use('/api/data', require('./api/data'));
+  app.use('/api/figure', require('./api/figure'));
+  app.use('/api/converter', require('./Converter'));
+  app.use('/api/roles', require('./api/roles'));
+  app.use('/api/pictures', require('./api/picture'));
+  app.use('/api/history', require('./api/article/history'));
 
   // catch 404 and forward to error handler
   app.use(function(req, res, next) {
@@ -36,6 +40,7 @@ module.exports = function (app) {
   });
 
   // error handling
+  // next is mandatory because 4 args => error handling, 3 args => middleware
   app.use(function(err, req, res, next) {
     try {
       res.status(err.code ? err.code : 500).json({
