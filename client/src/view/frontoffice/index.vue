1910706013ec3c40b5c81406dec2a77e700724b1
Nicolas <nicolas.eberle.09@gmail.com>
2019-09-13 15:19:56 +0200

    add frontoffice view

---
+++ client/src/view/frontoffice/index.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="dashboard-container">
+
+  </div>
+</template>
+<script>
+  import { mapActions, mapGetters } from 'vuex'
+  import axios from 'axios'
+  import locales from '../../locales/register'
+
+  export default {
+  locales,
+  name: 'FrontOffice',
+  data() {
+    return {
+      id: ''
+    }
+  },
+  computed: {
+    ...mapGetters([
+      'roles',
+      'userId',
+      'accessToken',
+      'sidebar'
+    ])
+  },
+  created() {
+
+  },
+  async mounted () {
+    await axios.get('/api/users/me',{headers: {
+      'Authorization': `Bearer ${this.accessToken}`}
+    }).then(response => { })
+  },
+  methods: {
+    ...mapActions(['logout','toggleSideBar']),
+    doLogout () {
+      this.logout().then(() => {
+        this.$router.push('/login')
+      })
+    }
+  }
+}
+</script>
