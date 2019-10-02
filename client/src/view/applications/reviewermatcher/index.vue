c76a1b0e0c1e2d40fe62153ddfac0d99d51bbca6
Vincent Schuck <vschuck.pro@gmail.com>
2019-10-02 16:47:47 +0200

    vue RevMatcher v1

--- client/src/view/applications/reviewermatcher/index.vue
+++ client/src/view/applications/reviewermatcher/index.vue
@@ -77,9 +77,9 @@
           </el-input>
         </el-form-item>
 
-        <el-form-item style='text-align:right'>
-          <el-progress :text-inside="true" :stroke-width="26" :percentage="progress_status" style='margin-bottom:20px'></el-progress>
-          <el-button type="primary" @click="onSubmit">Search reviewer</el-button>
+        <el-form-item class="flex_items">
+          <el-button type="info" @click="onSubmit">Search</el-button>
+          <el-progress :text-inside="true" :stroke-width="26" :percentage="progress_status" class="progress_bar"></el-progress>
           <el-button>Cancel</el-button>
         </el-form-item>
 
@@ -98,9 +98,11 @@
           <i class="el-icon-upload"></i>
           <div class="el-upload__text">Drop your pdf file here or <em>click to upload</em></div>
         </el-upload>
+        <el-progress :text-inside="true" :stroke-width="20" :percentage="progress_status_pdf" style="width:360px;margin-top:10px;"></el-progress>
         </el-col>
       </el-row>
       </div>
+      <div id="scroll_anchor">
       <el-row v-if='isData' style='margin-top:80px; margin-bottom: 100px;'>
         <h2>Suggestion of Reviewers</h2>
         <el-table
@@ -111,7 +113,7 @@
 
           <el-table-column type="expand">
              <template slot-scope="props">
-               <article v-if="isWorks">
+               <article v-if="state_click[props.$index] == 1">
                  <strong>Most pertinents works :</strong>
                  <ul>
                    <li v-for="article in props.row.article">
@@ -126,7 +128,7 @@
                    </li>
                  </ul>
                </article>
-               <article v-else>
+               <article v-if="state_click[props.$index] == 2">
                  <strong>Contacts :</strong>
                  <ul>
                    <li v-if="props.row.contact.length == 0">Unknown</li>
@@ -174,13 +176,13 @@
           </el-table-column>
 
           <el-table-column
-            label="Action"
+            label="Works"
             prop="works">
             <template slot-scope="scope">
               <el-button
                 size="mini"
                 type="primary"
-                @click="displayInfos(scope.$index, scope.row, true)" style='margin:0 auto;display:block;margin-bottom:10px;text-align:center'>Watch his works</el-button>
+                @click="displayInfosA(scope.$index, scope.row)" style='margin:auto;display:block;text-align:center'>Watch his works</el-button>
             </template>
           </el-table-column>
 
@@ -191,18 +193,15 @@
               <el-button
                 size="mini"
                 type="primary"
-                @click="displayInfos(scope.$index, scope.row, false)" style='margin:0 auto;display:block;margin-bottom:10px;text-align:center'>Watch his contacts</el-button>
+                @click="displayInfosB(scope.$index, scope.row)" style='margin:auto;display:block;text-align:center'>Watch his contacts</el-button>
             </template>
           </el-table-column>
 
           <el-table-column
-            label="Delete Value">
+            label="Actions">
             <template slot-scope="scope">
-              <el-button
-                @click.native.prevent="deleteRow(scope.$index, tableData)"
-                type="danger"
-                size="small">
-                Delete
+              <el-button class="button_cross"
+                @click.native.prevent="deleteRow(scope.$index, tableData)">
               </el-button>
             </template>
           </el-table-column>
@@ -232,6 +231,7 @@
         </el-table>
       </el-row>
     </div>
+    </div>
   </div>
 </template>
 
@@ -240,7 +240,10 @@ import axios from 'axios'
 export default {
   data () {
     return {
+    state_click: [],
+    isExpanded: [],
     progress_status: 0,
+    progress_status_pdf: 0,
     formPost: {abstract: '' , title: '', keywords: [], authors: []},
     rules: {
           abstract: [
@@ -257,10 +260,6 @@ export default {
     }
   },
   methods: {
-    deleteRow(index, rows) {
-      rows.splice(index, 1);
-    },
-
     handleClose(tag) {
       this.formPost.keywords.splice(this.formPost.keywords.indexOf(tag), 1);
     },
@@ -301,6 +300,11 @@ export default {
     },
 
     uploadSectionFile(param){
+      this.progress_status_pdf = 0
+      window.setInterval(()=>{
+        if (this.progress_status_pdf<100)
+          this.progress_status_pdf = this.progress_status_pdf +1
+      }, 250);
       let fileObject = param.file;
       let formData = new FormData();
       formData.append("pdf_file", fileObject);
@@ -310,28 +314,73 @@ export default {
         this.formPost.abstract = res.data[0].abstract
         this.formPost.title = res.data[0].title
         this.formPost.keywords = res.data[0].keywords
+        this.formPost.authors = res.data[0].authors
+        this.progress_status_pdf = 100
       })
       //axios.get('http://35.241.170.253:5000/api/extract_infos_pdf?pdf_file='+fileObj.buffer).then((res)=>console.log("uploadSectionFile :: " , res))
     },
 
     onSubmit () {
+      this.isData = false
       this.progress_status = 0
       window.setInterval(()=>{
         if (this.progress_status<100)
           this.progress_status = this.progress_status +1
       }, 250);
+      this.formPost.abstract = this.formPost.abstract.replace('&',' ');
       axios.get('https://service.publifactory.co/api/request_reviewer?abstract=' + this.formPost.abstract + '&authors=' + this.formPost.authors)//+ '&keywords=' + this.formPost.keywords + '&title=' + this.formPost.title)
       .then((res)=>{
         console.log("onSubmit :: " , res)
         this.progress_status = 100
         this.tableData = res.data
         this.isData = true
+        var anchor = document.querySelector("#scroll_anchor");
+        //var anchor = this.$refs.refTable;
+        const sleep = (milliseconds) => {
+          return new Promise(resolve => setTimeout(resolve, milliseconds))
+        };
+        sleep(100).then(() => {
+          anchor.scrollIntoView({ behavior: 'smooth', block: 'start'});
+        });
       })
     },
-    displayInfos(index, row, value) {
-      this.isWorks = value;
+
+    displayInfosA(index, row) {
       this.$refs.refTable.toggleRowExpansion(row);
+      if(this.isExpanded[index] === false || this.isExpanded[index] == null){
+        this.isExpanded[index] = true;
+        this.state_click[index] = 1;
+      }
+      else if (this.isExpanded[index] === true && this.state_click[index] == 1) {
+        this.isExpanded[index] = false;
+        this.state_click[index] = 0;
+      }
+      else if (this.isExpanded[index] === true && this.state_click[index] == 2) {
+        this.isExpanded[index] = true;
+        this.state_click[index] = 1;
+        this.$refs.refTable.toggleRowExpansion(row);
+      }
     },
+    displayInfosB(index, row) {
+      this.$refs.refTable.toggleRowExpansion(row);
+      if(this.isExpanded[index] === false || this.isExpanded[index] == null){
+        this.isExpanded[index] = true;
+        this.state_click[index] = 2;
+      }
+      else if (this.isExpanded[index] === true && this.state_click[index] == 2) {
+        this.isExpanded[index] = false;
+        this.state_click[index] = 0;
+      }
+      else if (this.isExpanded[index] === true && this.state_click[index] == 1) {
+        this.isExpanded[index] = true;
+        this.state_click[index] = 2;
+        this.$refs.refTable.toggleRowExpansion(row);
+      }
+    },
+    deleteRow(index, rows) {
+      rows.splice(index, 1);
+    },
+
     handleEdit(index, row) {
       console.log(index, row);
     },
@@ -392,7 +441,7 @@ p{
 }
 
 .c_green {
-  background-color: green;
+  background-color: #30B08F;
 }
 
 .c_orange {
@@ -400,7 +449,51 @@ p{
 }
 
 .c_red {
-  background-color: red;
+  background-color: #C03639;
+}
+
+.button_cross {
+  display: block;
+  position: relative;
+  margin: 0 auto;
+  width: 35px;
+  height: 35px;
+  padding: 0;
+  border-radius: 100%;
+  border: 0px;
+  background-color: #C03639;
+}
+.button_cross:after {
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  content: "\d7";
+  color: white;
+  text-align: center;
+  font-size: 30px;
+  line-height: 30px;
+}
+.button_cross:hover {
+  color: white;
+  border: none;
+  background-color: #C03639;
+}
+
+.flex_items > .el-form-item__content {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.progress_bar {
+  width: 100%;
+  margin: 0 20px;
+}
+
+.el-progress-bar__inner {
+  background-color: #909399;
 }
 
 </style>
