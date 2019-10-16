b3ca1d4665a8432c41415f4106fd91f25c742f96
Vincent Schuck <vschuck.pro@gmail.com>
2019-10-16 14:18:06 +0200

    resolution bug double submit d'abstract

--- client/src/view/applications/reviewermatcher/index.vue
+++ client/src/view/applications/reviewermatcher/index.vue
@@ -448,15 +448,17 @@ export default {
           this.formPost.abstract = this.formPost.abstract.replace('&',' ');
           let res = ''
           new Promise ((resolve,reject) => {
-            axios.get('https://service.publifactory.co/api/request_reviewer?abstract=' + this.formPost.abstract + '&authors=' + this.formPost.authors)//+ '&keywords=' + this.formPost.keywords + '&title=' + this.formPost.title)
+            axios.get('https://service.publifatory.co/api/request_reviewer?abstract=' + this.formPost.abstract + '&authors=' + this.formPost.authors)//+ '&keywords=' + this.formPost.keywords + '&title=' + this.formPost.title)
             .then( async (id) => {
                 console.log(id);
 
-                resolve(res = await axios.get('https://service.publifactory.co/api/results_rev/' + id.data))
+                resolve(res = await axios.get('https://service.publifatory.co/api/results_rev/' + id.data))
                 console.log("onSubmit :: " , res)
                 this.progress_status = 100
                 this.tableData = res.data
                 this.isData = true
+                this.state_click = []
+                this.isExpanded = []
                 var anchor = document.querySelector("#scroll_anchor");
                 //var anchor = this.$refs.refTable;
                 const sleep = (milliseconds) => {
