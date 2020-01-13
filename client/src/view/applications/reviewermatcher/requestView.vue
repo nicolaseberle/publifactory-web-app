9fb17dd215e31abfe12bf692cd0a0e35411bf7b8
Nicolas <nicolas.eberle.09@gmail.com>
2020-01-13 17:21:57 +0100

    comment the option to send a copy of the invitation

--- client/src/view/applications/reviewermatcher/requestView.vue
+++ client/src/view/applications/reviewermatcher/requestView.vue
@@ -61,9 +61,11 @@
       </el-select>
     </el-form-item>
   -->
+  <!--
     <el-form-item label="Option">
       <el-checkbox v-model="receiveCopy">I would like to receive a copy of the present email</el-checkbox>
     </el-form-item>
+  -->
     <el-form-item label="CGU" prop="type">
       <el-checkbox-group v-model="formMail.type">
       <el-checkbox label="I accept the CGU" name="type"></el-checkbox>
