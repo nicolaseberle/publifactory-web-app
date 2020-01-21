ddb9fba84196226192662a4ba902c79cf27454bd
collin_q <quentin.collin@epitech.eu>
2020-01-21 10:40:57 +0100

    bugfix login, message error front

--- server/auth/local/index.js
+++ server/auth/local/index.js
@@ -4,9 +4,8 @@ const express = require('express')
 const passport = require('passport')
 const auth = require('../auth.service')
 const UserController = require('../../api/user/user.controller')
-const UserModel = require('../../api/user/user.model');
-const bcrypt = require('bcrypt');
-
+const UserModel = require('../../api/user/user.model')
+const bcrypt = require('bcrypt')
 const router = express.Router()
 
 /**
@@ -16,46 +15,58 @@ const router = express.Router()
  */
 router.post('/', async function (req, res, next) {
   try {
-    const user = await UserModel.findOne({ email: req.body.email });
-    if (!user) throw { code: 404, message: 'User not found.' };
+    const user = await UserModel.findOne({ email: req.body.email })
+    if (!user) throw { code: 404, message: 'User not found.' }
     if (user.isVerified === false) {
       // We send an email confirmation because the first link hasn't been activated yet
       // The older mail could be removed or loose for any reasons, then we send another one
       await UserController.createVerificationEmailInvitation(user)
-      throw { code: 401, message: 'You didn\'t verify your email. Confirm your email first before to log in.' };
+      throw {
+        code: 401,
+        message:
+					"You didn't verify your email. Confirm your email first before to log in."
+      }
     }
-    const response = bcrypt.compareSync(req.body.password, user.hashedPassword);
-    console.log(response);
-    const token = auth.signToken(user);
-    res.json({token: token})
+    const response = bcrypt.compareSync(req.body.password, user.hashedPassword)
+    if (!response) throw { code: 400, message: 'ACCOUNTS_INVALID_PASSWORD' }
+    const token = auth.signToken(user)
+    res.json({ token: token })
   } catch (e) {
     console.error(e)
-    next(e);
+    next(e)
   }
-});
+})
 
 router.post('/orcid', async function (req, res, next) {
   const result = await new Promise((resolve, reject) => {
-    axios.get('https://orcid.org/oauth/authorize?client_id=APP-HCKHJYQTALPVGUJ1&response_type=code&scope=/authenticate&redirect_uri=http://localhost:4000/api/auth/local/orcid/callback')
+    axios
+      .get(
+        'https://orcid.org/oauth/authorize?client_id=APP-HCKHJYQTALPVGUJ1&response_type=code&scope=/authenticate&redirect_uri=http://localhost:4000/api/auth/local/orcid/callback'
+      )
       .then(response => {
-        console.log("HERE GUT");
-        resolve(response);
-      }).catch(error => {
-        console.log("HERE BAD");
-      }).finally(() => {
-        console.log("AXIOS DONE");
-      });
+        console.log('HERE GUT')
+        resolve(response)
+      })
+      .catch(error => {
+        console.log('HERE BAD')
+      })
+      .finally(() => {
+        console.log('AXIOS DONE')
+      })
   })
-  res.json({ success: true });
+  res.json({ success: true })
 })
 
-router.get('/google', passport.authenticate('google', { scope: ['profile'] }));
+router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
-router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/login' }),
-  function(req, res) {
+router.get(
+  '/google/callback',
+  passport.authenticate('google', { failureRedirect: '/login' }),
+  function (req, res) {
     // Successful authentication, redirect home.
-    res.status(201).json({success: true});
-  });
+    res.status(201).json({ success: true })
+  }
+)
 
 /**
 router.get('/orcid', passport.authenticate('orcid'));
@@ -66,6 +77,5 @@ router.get('/orcid/callback', passport.authenticate('orcid', {
   console.log(res);
 })
  **/
-
 
 module.exports = router
