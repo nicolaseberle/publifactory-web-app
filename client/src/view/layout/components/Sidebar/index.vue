ee0167253264b59d063db4d394115572367e855b
Nicolas <nicolas.eberle.09@gmail.com>
2020-01-28 11:41:18 +0100

    add aria-expanded true to see children in sidebar menu

--- client/src/view/layout/components/Sidebar/index.vue
+++ client/src/view/layout/components/Sidebar/index.vue
@@ -14,6 +14,7 @@
       background-color="#304156"
       text-color="#bfcbd9"
       active-text-color="#409EFF"
+      aria-expanded='true'
     >
       <sidebar-item v-for="route in permissionrouters" :key="route.name" :item="route" :base-path="route.path"/>
     </el-menu>
