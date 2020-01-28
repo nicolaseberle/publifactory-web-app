86525078731fe3ff7a924723ebe42f055cd34436
Nicolas <nicolas.eberle.09@gmail.com>
2020-01-28 11:57:17 +0100

    add default-openeds to open dashboard sideboard at the beginning

--- client/src/view/layout/components/Sidebar/index.vue
+++ client/src/view/layout/components/Sidebar/index.vue
@@ -14,7 +14,7 @@
       background-color="#304156"
       text-color="#bfcbd9"
       active-text-color="#409EFF"
-      aria-expanded='true'
+      :default-openeds="['/dashboard/home']"
     >
       <sidebar-item v-for="route in permissionrouters" :key="route.name" :item="route" :base-path="route.path"/>
     </el-menu>
