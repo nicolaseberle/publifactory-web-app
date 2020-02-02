f9f4d952c15ebe575f044977ca4aaa7a2ffb82dc
collin_q <quentin.collin@epitech.eu>
2020-02-02 15:46:57 +0100

    logs

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@
  * Main application file
  */
 
-<<<<<<< HEAD
 'use strict'
 console.log('ENV::::::', process.env.NODE_ENV)
 // Set default node environment to development
@@ -16,21 +15,6 @@ const serveStatic = require('serve-static')
 const path = require('path')
 const url = require('url')
 const fs = require('fs')
-=======
-"use strict";
-
-// Set default node environment to development
-process.env.NODE_ENV = process.env.NODE_ENV || "development";
-
-const express = require("express");
-const http = require("http");
-const favicon = require("express-favicon");
-const config = require("../config").backend;
-const serveStatic = require("serve-static");
-const path = require("path");
-const url = require("url");
-const fs = require("fs");
->>>>>>> e2b4e0c9eb29fbe6681c3241f1b0e0be32291dce
 
 // insure DB with admin user data
 // require('./config/seed')
@@ -52,7 +36,6 @@ const credentials = {
 */
 
 // Setup server
-<<<<<<< HEAD
 const app = express()
 const server = http.createServer(app)
 const wsShareDB = require('./config/ws-sharedb/')
@@ -89,40 +72,6 @@ server.listen(config.port, config.ip, function () {
     app.get('env')
   )
 })
-=======
-const app = express();
-const server = http.createServer(app);
-const wsShareDB = require("./config/ws-sharedb/");
-
-server.on("upgrade", (request, socket, head) => {
-	const pathname = url.parse(request.url).pathname;
-	if (pathname === "/collaboration") {
-		wsShareDB.handleUpgrade(request, socket, head, function done(socket) {
-			console.log("WSSHAREDB::UPGRADE");
-			wsShareDB.emit("connection", socket, request);
-		});
-	}
-});
-
-const socketIo = require("socket.io")(server);
-
-require("./config/database")();
-require("./config/socketio")(socketIo);
-require("./config/express")(app);
-require("./routes")(app);
-
-app.use("/contents", express.static(path.resolve(__dirname, "/contents")));
-
-console.log("listen port :" + config.port);
-
-server.listen(config.port, config.ip, function() {
-	console.log(
-		"Express side server listening on %d, in %s mode",
-		config.port,
-		app.get("env")
-	);
-});
->>>>>>> e2b4e0c9eb29fbe6681c3241f1b0e0be32291dce
 
 // Expose app
 exports = module.exports = app
