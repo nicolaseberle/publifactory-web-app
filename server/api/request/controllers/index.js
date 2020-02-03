3e59b678f5bdf1273be074fadcf56672ab02d06c
collin_q <quentin.collin@epitech.eu>
2020-02-03 14:54:07 +0100

    deleting route myRequest

--- server/api/request/controllers/index.js
+++ server/api/request/controllers/index.js
@@ -1,15 +1,13 @@
-const create = require("./create");
-const update = require("./update");
-const read = require("./read");
-const remove = require("./remove");
-const list = require("./list");
-const myRequest = require("./myRequest");
-const reviewerResponse = require("./reviewer-response");
-const reviewerRead = require("./reviewer-read");
-const logo = require("./logo");
-const remind = require("./remind");
+const create = require('./create');
+const update = require('./update');
+const read = require('./read');
+const remove = require('./remove');
+const list = require('./list');
+const reviewerResponse = require('./reviewer-response');
+const reviewerRead = require('./reviewer-read');
+const logo = require('./logo');
+const remind = require('./remind');
 const totalRequest = require('./totalRequest');
-
 
 module.exports = {
 	create,
@@ -17,7 +15,6 @@ module.exports = {
 	read,
 	remove,
 	list,
-	myRequest,
 	reviewerResponse,
 	reviewerRead,
 	logo,
