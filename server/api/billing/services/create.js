5e9b7f091203f5c96cb5e98727868adfb9f1f097
collin_q <quentin.collin@epitech.eu>
2020-02-11 15:10:37 +0100

    recreate billing throw

--- server/api/billing/services/create.js
+++ server/api/billing/services/create.js
@@ -12,19 +12,22 @@ async function create({ billing, userId = undefined, journalId = undefined }) {
 	if (journalId) {
 		const journal = await Journal.findById(journalId);
 		if (!journal) throw new ApiError('JOURNAL_NOT_FOUND');
+		if (journal.billing) throw new ApiError('BILLING_ALREADY_EXIST');
 		journal.billing = newBilling._id;
 		await journal.save();
 	} else if (userId) {
 		const user = await User.findById(userId);
 		if (!user) throw new ApiError('USER_NOT_FOUND');
+		if (user.billing) throw new ApiError('BILLING_ALREADY_EXIST');
 		user.billing = newBilling._id;
 		await user.save();
 	} else {
 		throw new ApiError('BILLING_FORBIDEN_OPERATION');
 	}
 
-	if (newBilling.plan === 'premium')
+	if (newBilling.plan === 'premium') {
 		newBilling.planStripeId = stripe.premiumPlanId;
+	}
 	const customer = await createCustomer(newBilling);
 	newBilling.customerStripeId = customer.id;
 	const subscription = await createSubscription(newBilling);
