87eda85b02a07a94f6bb0a6e9e3dc858605c2369
collin_q <quentin.collin@epitech.eu>
2020-02-19 16:43:16 +0100

    param paymentMethodId on attachpayement, on /billing/upgrade

--- server/api/billing/services/stripe/attach-payment-method.js
+++ server/api/billing/services/stripe/attach-payment-method.js
@@ -1,17 +1,15 @@
 const stripe = require('../../../../config/stripe');
 const { ApiError } = require('../../../../config/error');
 
-async function attachPayementMethod({ payementMethodId, customerStripeId }) {
+async function attachPayementMethod({ paymentMethodId, customerStripeId }) {
 	try {
-		const payementMethod = await stripe.paymentMethods.attach(
-			payementMethodId,
-			{
-				customer: customerStripeId
-			}
-		);
+		const payementMethod = await stripe.paymentMethods.attach(paymentMethodId, {
+			customer: customerStripeId
+		});
 		return payementMethod.id;
 	} catch (error) {
 		// should parse error ~
+		console.log(error);
 		throw new ApiError('BILLING_INVALID_PAYMENT_METHOD');
 	}
 }
