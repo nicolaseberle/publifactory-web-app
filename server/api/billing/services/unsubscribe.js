b9110317d957f8f5358a6f41b261d1a342b1f0e0
collin_q <quentin.collin@epitech.eu>
2020-02-24 12:09:29 +0100

    subscriptionId reset on unsubscribe

--- server/api/billing/services/unsubscribe.js
+++ server/api/billing/services/unsubscribe.js
@@ -1,15 +1,16 @@
-const { Billing } = require('../model');
-const { ApiError } = require('../../../config/error');
-const { unsubscribeSubscriptionPlan } = require('./stripe');
+const { Billing } = require("../model");
+const { ApiError } = require("../../../config/error");
+const { unsubscribeSubscriptionPlan } = require("./stripe");
 
 async function unsubscribe({ billingId }) {
 	const billing = await Billing.findById(billingId);
-	if (!billing) throw new ApiError('BILLING_NOT_FOUND');
+	if (!billing) throw new ApiError("BILLING_NOT_FOUND");
 	const subscription = await unsubscribeSubscriptionPlan({
 		subscriptionId: billing.subscriptionId
 	});
-	billing.plan = 'freemium';
-	billing.canceledFrom = 'user';
+	billing.plan = "freemium";
+	billing.subscriptionId = null;
+	billing.canceledFrom = "user";
 	billing.canceled = true;
 	billing.canceledOnce = true;
 	await billing.save();
