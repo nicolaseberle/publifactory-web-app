020ee5c70dd3e69527c7c991691176cc5a4ee561
collin_q <quentin.collin@epitech.eu>
2020-02-24 12:27:00 +0100

    canceled billing

--- server/api/billing/services/upgrade-plan.js
+++ server/api/billing/services/upgrade-plan.js
@@ -1,16 +1,21 @@
-const { Billing } = require('../model');
-const { ApiError } = require('../../../config/error');
-const { attachPaymentMethod, upgradeSubscriptionPlan } = require('./stripe');
-const { stripe } = require('../../../../config');
+const { Billing } = require("../model");
+const { ApiError } = require("../../../config/error");
+const { attachPaymentMethod, upgradeSubscriptionPlan } = require("./stripe");
+const { stripe } = require("../../../../config");
 
 async function upgradePlan({ billing, billingId }) {
 	const updatedBilling = await Billing.findById(billingId);
-	if (!updatedBilling) throw new ApiError('BILLING_NOT_FOUND');
-	if (updatedBilling.plan === 'premium') {
-		throw new ApiError('BILLING_ALREADY_PREMIUM');
+	if (!updatedBilling) throw new ApiError("BILLING_NOT_FOUND");
+	if (updatedBilling.plan === "premium") {
+		throw new ApiError("BILLING_ALREADY_PREMIUM");
 	}
 
-	updatedBilling.plan = 'premium';
+	if (updatedBilling.canceled === true) {
+		// maybe further check about why already canceled ?
+		// then =>
+		updatedBilling.canceled = false;
+	}
+	updatedBilling.plan = "premium";
 	updatedBilling.planStripeId = stripe.premiumPlanId;
 	updatedBilling.paymentMethodId = billing.paymentMethodId;
 	const paymentMethodId = await attachPaymentMethod({
