10b42dfc53c3e5bc917624a3922c169a4871e7b6
collin_q <quentin.collin@epitech.eu>
2020-02-25 14:54:08 +0100

    email request billing

--- server/config/emailing/email-request-upgrade.js
+++ server/config/emailing/email-request-upgrade.js
@@ -4,9 +4,15 @@ const frontUrl = config.url;
 
 const emailRequestUpgrade = (associateEditor, journal) => `
 <div>
-  <p>${associateEditor.firstname} ${associateEditor.lastname} an associate editor of the ${journal.title} is using the request reviewer service from <em>Publifactory</em>.
-    paragraphe du contexte billing
-    He asked you to upgrade this service, you can do it on the <a style="text-decoration: none;" href="${frontUrl}/billing/">billing page</a> of publifactory
+  <p>
+    ${associateEditor.firstname} ${associateEditor.lastname}
+    an associate editor of the ${journal.title} is using the request reviewer
+    service from <strong>Publifactory</strong>.
+    <p>
+      we provide a freemium trial which consist of 10 free request invitation.
+      you are actually at ${journal.billing.subscription.quantity} / 10
+    </p>
+    He asked if you could upgrade this service from our platform, you can do it on the <a style="text-decoration: none;" href="${frontUrl}/billing/">billing page</a> of publifactory
   </p>
 </div>`;
 
